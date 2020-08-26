7b03ad6a0936a11bec3ed2e4be1f0c4ca5905342
JamesLeggero <james.m.leggero@gmail.com>
2020-08-26 17:21:48 -0400

    connection setups written, not tested. schema in progress

---
+++ server.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+
+//ex and mdb setup
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const db = mongoose.connection
+const methodOverride = require('method-override')
+
+const PORT = process.env.PORT || 3000
+
+//mw
+app.use(express.static('public')) //for grabbing/linking css and js
+app.use(express.urlencoded({extended : false})) //for req.body
+app.set('view engine', 'jsx') //to dictate our VE language
+app.engine('jsx', require('express-react-views')).createEngine()
+app.use(methodOverride('_method')) //to utilize patch, put, delete routes
+
+//db
+const mongoURI = process.env.MONGO_URI
+mongoose.connect(mongoURI, { useNewUrlParser : true, useUnifiedTopology : true, useFindAndModify : true })
+db.once('open', ()=>{
+    console.log('mongo connected')
+})
+
+const Todo = require('./models/todos')
+
+app.get('/', (req, res)=>{
+    res.send('static up')
+})
+
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log('listening')
+})
